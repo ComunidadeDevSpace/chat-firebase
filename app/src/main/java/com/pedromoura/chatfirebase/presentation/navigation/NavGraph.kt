a5f2c808e88036a1a9aa75985eec20690e35fe89
Pedro Moura <pedromoura@MacBook-Pro-de-Pedro.local>
2024-08-04 23:36:06 -0300

    Implementação da tela de Chat

--- app/src/main/java/com/pedromoura/chatfirebase/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/pedromoura/chatfirebase/presentation/navigation/NavGraph.kt
@@ -1,10 +1,15 @@
 package com.pedromoura.chatfirebase.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.FirebaseDatabase
+import com.pedromoura.chatfirebase.presentation.chat.ChatScreen
+import com.pedromoura.chatfirebase.presentation.chat.ChatViewModel
 import com.pedromoura.chatfirebase.presentation.login.LoginScreen
 import com.pedromoura.chatfirebase.presentation.login.LoginViewModel
 
@@ -16,12 +21,15 @@ sealed class Screen(val route: String) {
 @Composable
 fun NavGraph(startDestination: String = Screen.Login.route) {
 
-    val navController = rememberNavController()
+    FirebaseApp.initializeApp(LocalContext.current)
+    val database = FirebaseDatabase.getInstance()
 
+    val navController = rememberNavController()
     val viewModel = LoginViewModel(LocalContext.current)
+    val chatViewModel = ChatViewModel(database, LocalContext.current)
 
     NavHost(navController = navController, startDestination = startDestination ) {
         composable(Screen.Login.route) { LoginScreen(viewModel, navController = navController) }
-        composable(Screen.Chat.route) { /* Chamar a Screen de Chat */  }
+        composable(Screen.Chat.route) { ChatScreen(chatViewModel)  }
     }
 }
