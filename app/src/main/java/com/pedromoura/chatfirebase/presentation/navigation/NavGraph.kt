8900b32fd930861aa108ddc6822d0e8d11f693fc
Pedro Moura <pedromoura@MacBook-Pro-de-Pedro.local>
2024-08-04 22:08:18 -0300

    Implementação da tela de Login + Viewmodel

--- app/src/main/java/com/pedromoura/chatfirebase/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/pedromoura/chatfirebase/presentation/navigation/NavGraph.kt
@@ -1,9 +1,12 @@
 package com.pedromoura.chatfirebase.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.pedromoura.chatfirebase.presentation.login.LoginScreen
+import com.pedromoura.chatfirebase.presentation.login.LoginViewModel
 
 sealed class Screen(val route: String) {
     object Login : Screen("login")
@@ -15,8 +18,10 @@ fun NavGraph(startDestination: String = Screen.Login.route) {
 
     val navController = rememberNavController()
 
+    val viewModel = LoginViewModel(LocalContext.current)
+
     NavHost(navController = navController, startDestination = startDestination ) {
-        composable(Screen.Login.route) { /* Chamar a Screen de Login */ }
+        composable(Screen.Login.route) { LoginScreen(viewModel, navController = navController) }
         composable(Screen.Chat.route) { /* Chamar a Screen de Chat */  }
     }
 }
